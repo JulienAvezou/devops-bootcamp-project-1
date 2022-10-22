44fff267f0ff837c1062bd9c6c7d37aa5239987e
JulienAvezou <62488871+JulienAvezou@users.noreply.github.com>
2022-10-22 13:53:40 +0200

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,9 +1,15 @@
 def buildJar() {
     echo 'building the application...'
+    sh 'mvn package'
 } 
 
 def buildImage() {
-    echo 'testing the application...'
+    echo 'building the Docker image...'
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t julienavezou/my-repo:jma-1.1 .'
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh 'docker push julienavezou/my-repo:jma-1.1'
+    }
 } 
 
 def deployApp() {
